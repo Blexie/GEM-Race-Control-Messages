f9338ef3557f5e68fccd9bc82739e8a5e56170a6
Blexie <fraser.blexie@gmail.com>
2022-06-17 23:17:46 +0100

    Yes

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ sys.path.append('../')
 
 sheeturl = input("Google sheet URL: ")
 worksheet = input("Name of worksheet with messages: ")
+obsport = input("OBS Websocket plugin port: (default: 4444)")
 obspassword = input("OBS Websocket plugin password: ")
 gc = pygsheets.authorize(local=True)
 sh = gc.open_by_url(str(sheeturl))
@@ -17,22 +18,26 @@ gws = sh.worksheet_by_title(str(worksheet))
 
 
 host = "localhost"
-port = 4444
+if obsport == '':
+    port = 4444
+else:
+    port = str(obsport)
 password = str(obspassword)
 
 ws = obsws(host, port, password)
 
-
 while True:
     ws.connect()
+    scene = ws.call(requests.GetCurrentScene()).getName()
+    if scene != "In-Race":
+        continue
     entries = gws.range('A:A')
     try:
         for column in entries:
             for cell in column:
-                if cell.value == '' or gws.cell("B"+str(cell.row)).value == "Broadcasted":
+                if cell.value == '' or cell.value == 'DECISION' or gws.cell("B"+str(cell.row)).value == "Broadcasted":
                     continue
                 text = cell.value
-                ws.call(requests.SetCurrentScene("In-Race"))
                 ws.call(requests.SetTextGDIPlusProperties(source="Race Control Message", text=text))
                 ws.call(requests.SetSceneItemProperties(scene_name="In-Race", item="Race Control Message", visible=True))
                 time.sleep(10)
