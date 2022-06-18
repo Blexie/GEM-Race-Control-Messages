f0202216ca104eef45cda162677153e7d0e59816
Blexie <fraser.blexie@gmail.com>
2022-06-18 02:09:57 +0100

    Changed to config file rather than user prompt, sample config included. Cleaned up some operation (checking for correct scene amongst other things)

---
+++ config.py
@@ -0,0 +1,5 @@
+# Sample config
+sheeturl = "https://spreadsheet.link"
+worksheet = "Sheet1"
+obsport = "4444"
+obspassword = "Password123"
--- main.py
+++ main.py
@@ -3,51 +3,46 @@ import time
 import pygsheets
 from obswebsocket import obsws, requests
 import logging
+import config
 
 logging.basicConfig(level=logging.ERROR)
 
 sys.path.append('../')
 
-sheeturl = input("Google sheet URL: ")
-worksheet = input("Name of worksheet with messages: ")
-obsport = input("OBS Websocket plugin port: (default: 4444)")
-obspassword = input("OBS Websocket plugin password: ")
+sheeturl = config.sheeturl
+worksheet = config.worksheet
+obsport = config.obsport
+obspassword = config.obspassword
 gc = pygsheets.authorize(local=True)
 sh = gc.open_by_url(str(sheeturl))
 gws = sh.worksheet_by_title(str(worksheet))
-
+print("Race Control Alerts running...")
 
 host = "localhost"
-if obsport == '':
-    port = 4444
-else:
-    port = str(obsport)
-password = str(obspassword)
 
-ws = obsws(host, port, password)
+ws = obsws(host, obsport, obspassword)
 
 while True:
     ws.connect()
     scene = ws.call(requests.GetCurrentScene()).getName()
-    if scene != "In-Race":
-        continue
-    entries = gws.range('A:A')
-    try:
-        for column in entries:
-            for cell in column:
-                if cell.value == '' or cell.value == 'DECISION' or gws.cell("B"+str(cell.row)).value == "Broadcasted":
-                    continue
-                text = cell.value
-                ws.call(requests.SetTextGDIPlusProperties(source="Race Control Message", text=text))
-                ws.call(requests.SetSceneItemProperties(scene_name="In-Race", item="Race Control Message", visible=True))
-                time.sleep(10)
-                ws.call(requests.SetSceneItemProperties(item="Race Control Message", visible=False))
-                gws.cell("B"+str(cell.row)).value = "Broadcasted"
-                time.sleep(3)
-            #info=ws.call(requests.GetTextGDIPlusProperties(source="Race Control Message"))
-            #print(info)
+    if scene == "In-Race":
+        entries = gws.range('A:A')
+        try:
+            for column in entries:
+                for cell in column:
+                    if cell.value == '' or cell.value == 'DECISION' or gws.cell("B"+str(cell.row)).value == "Broadcasted":
+                        continue
+                    text = cell.value
+                    ws.call(requests.SetTextGDIPlusProperties(source="Race Control Message", text=text))
+                    ws.call(requests.SetSceneItemProperties(scene_name="In-Race", item="Race Control Message", visible=True))
+                    time.sleep(60)
+                    ws.call(requests.SetSceneItemProperties(item="Race Control Message", visible=False))
+                    gws.cell("B"+str(cell.row)).value = "Broadcasted"
+                    time.sleep(10)
 
-    except KeyboardInterrupt:
+        except KeyboardInterrupt:
+            pass
+    else:
         pass
 
     ws.disconnect()
